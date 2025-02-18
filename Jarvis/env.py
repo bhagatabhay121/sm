51d439733bc41faed7a6caad2ca02bfcb9b821b9
bhagatabhay121 <bhagatabhay121@gmail.com>
2025-02-18 12:33:04 +0530

    Create env.py

---
+++ Jarvis/env.py
@@ -0,0 +1,9 @@
+# .env
+
+CohereAPIKey = 7xcAOv4godZ88wUJ0KGDssp5b2KVgLS2JsdwO4K9
+Username = Abhay
+Assistantname = Jarvis
+GroqAPIKey = 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
+InputLanguage = en
+AssistantVoice = en-CA-LiamNeural
+HuggingFaceAPIKey = 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
